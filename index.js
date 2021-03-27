1537d1b6d2222f8b850febb212400c07dd50f34b
nikobez <nikobez@yandex.ru>
2021-03-27 22:13:43 +0300

    Day 2

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from 'express'
+import path from 'path'
+//import {requestTime, logger} from './middlewares.js'
+import serverRoutes from './routes/servers.js'
 
 const PORT = 3333
 const application = express()
+const __dirname = path.resolve()
 
-application.get('/', (wreq,wres) => {
-    wres.send('<h1>Express server</h1>')
+application.set('view engine',"ejs")
+application.set('views', path.resolve(__dirname,'templates'))
 
+//application.use(express.static(path.resolve(__dirname,'static')))
+//application.use(requestTime)
+//application.use(logger)
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname,'static'))
+// })
+
+application.use(serverRoutes)
+
+application.get('/', (req, res) => {
+    res.render('index', {title: 'Main page', active: 'main', servport: PORT})
+})
+
+application.get('/upload', (req, res) => {
+    res.render('upload', {title: 'Upload page', active: 'upload', servport: PORT})
 })
 
 application.listen(PORT, () => {
---
+++ routes/servers.js
@@ -0,0 +1,9 @@
+import {Router} from 'express'
+
+const router = Router()
+
+router.get ('/api/server',(req,res) => {
+    res.json({test: 42})
+})
+
+export default router
