d56d1727cf1bb31d97eb011b7640453e6bfaf42d
nikobez <nikobez@yandex.ru>
2021-03-28 12:30:42 +0300

    Day 3. Uploading file added.

--- controllers/servers.js
+++ controllers/servers.js
@@ -1,3 +1,9 @@
 export const getAll = (req, res) => {
-    
+    res.status(200)
+    res.json({id: 1,text: 'file'})
+}
+
+export const postFile = (req,res) => {
+    res.status(200)
+    res.json({ok: 1})
 }
